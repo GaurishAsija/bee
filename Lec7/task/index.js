0b9a098e68c65099a0478d36688cb031a0bbdd82
GaurishAsija <asijagaurish@gmail.com>
2025-07-16 14:54:26 +0530

    new

--- Lec7/task/index.js
+++ Lec7/task/index.js
@@ -1,18 +1,17 @@
 const fs = require('fs');
-let users=[
+let users = [
     {
-        name: "Gaurish",
+        name: "Yashpal",
         age: 20,
         address : "Bangalore",
     },
     {
-        name: "Gaurav",
+        name: "Vansh",
         age: 22,
         address : "Bangalore",
     }
-
 ]
-fs.writeFile('../users.txt',JSON.stringify(users),function(err) {
+fs.writeFile('../users1.txt',JSON.stringify(users),function(err) {
     if (err) return console.error(err);
     console.log('Done');
 });
