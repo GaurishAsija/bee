d97c513fb1e8db17d561b97bfb87d7476e7131fe
GaurishAsija <asijagaurish@gmail.com>
2025-07-21 15:08:06 +0530

    new

---
+++ Lec8/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.get('/', (req, res) => {
+    // res.send("Hello"); //can send string and html also and response header set hoke jata hai 
+
+//   res.sendFile(__dirname + '/index.html');
+
+//   res.json({
+//     name: 'Gaurish',
+//     age: 20,
+//   })
+res.end("hi"); //can only send string and response header set nahi hoga
+  
+});
+
+//path-variable
+//1. query parameter
+app.get('/watch', (req, res) => {
+  let  videoId = req.query.v;
+  res.send("got it");
+  console.log(videoId);
+});
+
+app.listen(3000, function() {
+  console.log('server started');
+});
